fb0d7427ad7a320d6329910ef10edbdfa0b2f2d1
Blossom Coryat <blossom.coryat@gmail.com>
2014-12-24 22:39:49 -0500

    updated front page layout and style

--- front-page.php
+++ front-page.php
@@ -18,7 +18,19 @@
 		</article>
 		<!-- End Jumbotron image -->
 
-		
+		<!-- DD description -->
+		<article class="dddesc">
+        	<div class="container">	
+        	  <div class="row">
+        	  	<div class="col-md-6 col-md-offset-3 text-center">
+		         <p class="lead  text-center">Doggedly Devoted is dog daycare that is thoughtfully designed for your dog's safety, stimulation, and socialization. 
+		         We are professionally trained dog play specialists who take exceptional care of your canine family member.</p>
+	            </div>
+	          </div>
+	        </row>
+        </article>
+
+		<!-- end DD descriptions -->
 
 
 		
@@ -59,9 +71,9 @@
 				<div class="row">
 					<div class="col-xs-12"><h2 class="header">From the Dog Blog</h2></div>
 				</div>
-				
+				<div class="row">   
 				<?php
-                      $args = array( 'posts_per_page' => 1, 'post_type' => 'post' , 'offset' => 0);
+                      $args = array( 'posts_per_page' => 2, 'post_type' => 'post' , 'offset' => 0);
                       $myposts = get_posts( $args );
                       foreach ( $myposts as $post ) : setup_postdata( $post ); 
 					  $attr = array('class'	=> "thumbnail pull-left",
@@ -72,25 +84,19 @@
 	
 							
 
-				<div class="row">
-				
-						
-					    
-					    <div class="col-xs-12">
-					    	
-					    	  
+				  
+					    <div class="col-md-6 dogblogteaser">
 						       <h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
-						       <p><?php the_date(); ?><i class="icon-dot"></i>  by  <?php the_author(); ?>  </p>
+						       <p><?php the_date(); ?></p>
 						       <p><?php the_post_thumbnail('medium',$attr); ?>
 						        <?php the_excerpt(); ?>
-						       <a href="<?php the_permalink();?>" class="btn btn-default">Read More</a>
-						
+						       <a href="<?php the_permalink();?>" class="btn btn-default">Read More</a>			
 				        </div>
 						
-					</div>
-					<?php endforeach;?> 
+				
+			    <?php endforeach;?> 
 					
-				 </div>
+				</div> 
 
 			</div>     
 		</article>
