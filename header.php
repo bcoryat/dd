50b3b4bfc7f35d432f915f425670753befcb76cf
Blossom Coryat <blossom.coryat@gmail.com>
2016-12-07 23:52:48 -0500

    add facebook pixel code

--- header.php
+++ header.php
@@ -23,6 +23,26 @@
 				<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 			<![endif]-->
 			 <?php wp_head(); ?>
+               <!-- Facebook pixel  -->
+              <!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '1699855016895475'); 
+fbq('track', 'PageView');
+</script>
+<noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id=1699855016895475&ev=PageView
+&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
 	</head>
 	<body>
     	<!-- Header -->
