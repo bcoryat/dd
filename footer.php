8d4ed77c7240c0fea67fc7d34f5b8d4a2879224f
Blossom Coryat <blossom.coryat@gmail.com>
2017-12-19 09:46:28 -0500

    adding psp link

--- footer.php
+++ footer.php
@@ -2,43 +2,36 @@
 <!-- Footer -->
 <footer>
 
-	<!-- begin Contact -->
-	<a id="contactinfo"></a>
-	<article class="contact">
-	
-	<div id="contact_bg">
-		<div class="container">
-			 
-			
+    <!-- begin Contact -->
+    <a id="contactinfo"></a>
+    <article class="contact">
+        <div id="contact_bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-9">
+                            <address>
+                            <h2>Contact Us</h2>
+                            <i class="icon-phone"></i><a href="tel:+12124267232">212 426 7232</a><br />
+                            <i class="icon-mail"></i> <a href="mailto:info@doggedlydevoted.com">info@doggedlydevoted.com</a><br />
+                            <i class="icon-facebook"></i> <a href="https://www.facebook.com/doggedlydevoted">facebook.com/DoggedlyDevoted</a><br />
+                            <i class="icon-instagram"></i> <a href="https://www.instagram.com/doggedlydevoted">instagram.com/DoggedlyDevoted</a><br />
+                            <i class="icon-twitter"></i> <a href="https://twitter.com/DoggedlyDevoted">twitter.com/DoggedlyDevoted</a><br />
+                            </address>
+                     </div>
+                     <div class="col-sm-3">
+                         <p align="left"><b>Powered By </b></p><a href="http://www.petsitter-software.com/" target="_blank" rel="nofollow"><img src="https://static-petsoftware-net.s3.amazonaws.com/petsitterplus/logo.png" width="180" height="51" align="left" /></a></p>
+                     </div>
 
-			<div class="row">
-			<div class="col-sm-12">
-				    
-					<address>
-					<h2>Contact Us</h2>
-					<i class="icon-phone"></i><a href="tel:+12124267232">212 426 7232</a><br />
-					<i class="icon-mail"></i> <a href="mailto:info@doggedlydevoted.com">info@doggedlydevoted.com</a><br />
-					<i class="icon-facebook"></i> <a href="https://www.facebook.com/doggedlydevoted">facebook.com/DoggedlyDevoted</a><br />
-					<i class="icon-instagram"></i> <a href="https://www.instagram.com/doggedlydevoted">instagram.com/DoggedlyDevoted</a><br />
-					<i class="icon-twitter"></i> <a href="https://twitter.com/DoggedlyDevoted">twitter.com/DoggedlyDevoted</a><br />
-					</address>
-             </div>
+                </div> 
+            </div>
+        </div>
+        </div><!--??-->
+    </article>
+    <!-- end Contact -->
 
-			</div> 
-
-
-
-			
-			 </div>
-
-		 </div>
-	</div>
-	</article>
-	<!-- end Contact -->
-
-	<div class="bottom-foot">
-		<p>&copy; 2017 Doggedly Devoted. All rights reserved.</p>
-	</div>
+    <div class="bottom-foot">
+        <p>&copy; 2017 Doggedly Devoted. All rights reserved.</p>
+    </div>
 
 </footer>   
 <!-- end Footer --> 
